f954bb2d5b27b834011406ac963f262ceba121b8
Faizon Williams <faizon@protonmail.com>
2020-05-05 06:29:41 -0400

    Added line numbers for errors

--- shell.py
+++ shell.py
@@ -4,7 +4,7 @@ import basic
 #Read input From Terminal Window
 while True:
     text = input('basic > ')
-    result, error = basic.run(text)
+    result, error = basic.run('<stdin>',text)
 
     if error: print(error.as_string())
     else: print(result)
